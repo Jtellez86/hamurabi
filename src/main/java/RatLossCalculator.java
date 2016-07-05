8fdd61ad22f208c844306444028c494fdb3f2586
Joel Tellez <Jtellez@thoughtworks.com>
2016-07-04 21:56:22 -0500

    Implemented calculator to determine how much food was lost to rats.

---
+++ src/main/java/RatLossCalculator.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class RatLossCalculator {
+
+  Random numberGenerator;
+
+  public RatLossCalculator() {
+    this.numberGenerator = new Random();
+  }
+
+  public Integer calculateLoss() {
+    return numberGenerator.nextInt(100);
+  }
+}
---
+++ src/test/java/RatLossCalculatorTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RatLossCalculatorTest {
+
+  @Test
+  public void shouldReturnValueBetween0and100(){
+
+    RatLossCalculator calculator = new RatLossCalculator();
+
+    int lostBushels = calculator.calculateLoss();
+
+    assertThat(lostBushels).isBetween(0, 100);
+
+  }
+}
